94f856d1629544a28ba29f83b6346c9dada6c239
bes <iliabes>
2022-04-02 18:32:57 +0500

    problem witch routes

--- src/Headers/Header.js
+++ src/Headers/Header.js
@@ -1,7 +1,10 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import '../App.css';
 import TopNavButton from '../topnavButton/topnavButtin';
+import Content from '../Content/Content.js';
 import './Headers.sass'
+import Timer2 from'../Timer2/Timer2.js'
 import {FaPlay} from 'react-icons/fa';
 import {FaCog} from 'react-icons/fa';
 import {FaGrav} from 'react-icons/fa';
@@ -16,13 +19,23 @@ function Header() {
         <TopNavButton icon={<FaGrav/>}></TopNavButton>
       </div>
       <div className='nav-button'>
-        <TopNavButton icon={<FaPlay/>} ></TopNavButton>
-        <TopNavButton icon={<FaAdjust/>}></TopNavButton>
+
+        <TopNavButton  icon={<FaPlay/>}></TopNavButton>
         <TopNavButton icon={<FaInfo/>}></TopNavButton>
         <TopNavButton icon={<FaCog/>}></TopNavButton>
+        <TopNavButton icon={<FaAdjust/>}></TopNavButton>
       </div> 
+
+      {/* <BrowserRouter>
+      <Routes>
+       <Route path="/" element={<Content />} />
+       <Route path="/sek" element={<Timer2 />} />
+      </Routes>
+    </BrowserRouter> */}
+
     </div>
   )
 }
 
 export default Header;
+{/*  */}
---
+++ src/Pomodoro/Pomodoro.js
@@ -0,0 +1,19 @@
+
+import './Content.sass';
+import Circle from '../Circle/Circle.js'
+import TimerCont from '../TimerCont/TimerCont'
+import {FaPlay} from 'react-icons/fa';
+
+function Header() {
+  return (
+    <div className="timer2">
+
+      <h1>This wil be Pomodoro</h1>
+    </div>
+  );
+}
+
+export default Header;
+
+
+// B09F9F
